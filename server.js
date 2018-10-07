ed0b8dcfa6787c55f260d515e989de6c0bb07ad0
Thrivikram Mudunuri <23713701+tmudunuri@users.noreply.github.com>
2018-10-08 00:55:29 +0530

    Fix

--- server.js
+++ server.js
@@ -53,9 +53,13 @@ app.use(passport.session());
 
 
 app.get('/login/github',
-  passport.authenticate('github'));
+  passport.authenticate('github', { scope: [ 'user:email' ] }),
+  function(req, res){
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  });
 
-app.get('/login/github/return', 
+app.get('/auth/github/callback', 
   passport.authenticate('github', { failureRedirect: '/' }),
   function(req, res) {
     res.redirect('/');
